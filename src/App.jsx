9203cd7ba4a324b19b98a27fd11c6044333db17b
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2025-09-20 22:45:18 +0530

    Home page components are implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Navbar } from "./component/Navbar/Navbar";
 import { ThemeProvider } from "@emotion/react";
 import { darkTheme } from "./Theme/DarkTheme";
 import { CssBaseline } from "@mui/material";
+import { Home } from "./component/Home/Home";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar />
+      <Home/>
     </ThemeProvider>
   );
 }
---
+++ src/component/Home/topMeal.js
@@ -0,0 +1,49 @@
+
+export const topMeals=[
+
+    {
+        image:"https://images.pexels.com/photos/13814644/pexels-photo-13814644.jpeg",
+        title:"Pizza"
+    },
+    {
+        image:"https://images.pexels.com/photos/327158/pexels-photo-327158.jpeg",
+        title:"Burger"
+    },
+    {
+        image:"https://images.pexels.com/photos/32447060/pexels-photo-32447060.jpeg",
+        title:"Sandwich"
+    },
+    {
+        image:"https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg",
+        title:"Cake"
+    },
+    {
+        image:"https://images.pexels.com/photos/12737656/pexels-photo-12737656.jpeg",
+        title:"Biryani"
+    },
+    {
+        image:"https://images.pexels.com/photos/6646369/pexels-photo-6646369.jpeg",
+        title:"Rolls"
+    },
+    {
+        image:"https://images.pexels.com/photos/5718025/pexels-photo-5718025.jpeg",
+        title:"Chicken"
+    },
+    {
+        image:"https://images.pexels.com/photos/5560763/pexels-photo-5560763.jpeg",
+        title:"Dhosa"
+    },
+    {
+        image:"https://images.pexels.com/photos/3625371/pexels-photo-3625371.jpeg",
+        title:"Ice-cream"
+    },
+    {
+        image:"https://images.pexels.com/photos/3727196/pexels-photo-3727196.jpeg",
+        title:"Fried rice"
+    },
+    {
+        image:"https://images.pexels.com/photos/7426867/pexels-photo-7426867.jpeg",
+        title:"Beef rice"
+    }
+    
+]
